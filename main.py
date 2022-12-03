11ee70ae94712107ee016eb1d6db5bd704ec143c
Michael slyusar <michael.slyusar@gmail.com>
2022-12-03 22:28:18 +0200

    Adding Description and requirments from description page

--- main.py
+++ main.py
@@ -1,43 +1,73 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import time
 
 allJobs = []
 
 # #########################add into class
 # Supporting functions
 #
-def scrapeCategory(category):  
+
+
+def scrapeCategory(category):
     catLink = category.find('a').get('href')
-    # print(catLink)
     scrapePage(catLink)
-# 
+#
+
+
 def scrapePage(catLink):
-    pageSource = requests.get(catLink,headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}).text
+    pageSource = requests.get(catLink, headers={
+                              "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}).text
     pageSoup = BeautifulSoup(pageSource, 'html.parser')
+
     pageJobs = pageSoup.find_all('tr', class_='job-row')
-    
-    # jobs = []
+    # print('after find  '+ str(time.perf_counter()))
     for job in pageJobs:
         jobColumns = job.find_all('td')
 
-        jobLink = job.find('a').get('href')
+        jobLink = 'https://elbitsystemscareer.com' + job.find('a').get('href')
         jobTitle = job.find('a').string
 
         jobCode = jobColumns[1].string
         jobRegion = jobColumns[2].string
-        jobCity = jobColumns[3].contents[0].replace(' ','')
+        jobCity = jobColumns[3].contents[0].replace(' ', '')
 
-        allJobs.append({'Title':jobTitle,'Code':jobCode,'Link':jobLink,'Region':jobRegion,'City':jobCity})
+        jobObject = {'Title': jobTitle, 'Code': jobCode,
+                     'Link': jobLink, 'Region': jobRegion, 'City': jobCity}
+        # Inside job description page
+        print('Before requests: ' + str(time.perf_counter()))
+        jdSource = requests.get(jobLink, headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+        print(jdSource.elapsed.total_seconds())
+        print('after requests before soup: ' + str(time.perf_counter()))
+        jdSoup = BeautifulSoup(jdSource, 'html.parser')
+        print('after soup: ' + str(time.perf_counter()))
+        jdJobBoxes = jdSoup.find_all('div', class_="box-job")
 
+        for jb in jdJobBoxes:
+            jbJobBoxHeader = jb.find('h2').text.lstrip()
+            jbJobBoxDvis = jb.find_all('div')
+            jbJobBoxText = []
+            for div in jbJobBoxDvis:
+                jbJobBoxText.append(div.text)
+            jobObject[jbJobBoxHeader] = jbJobBoxText
+            print('after job description page: ' + str(time.perf_counter()))
 
+        # Add to global job array
+        allJobs.append(jobObject)
+
+    # Class of next button if available: 'next back-steps enabled'
+    try:
+        pageNextBtn = pageSoup.find('a', 'next back-steps enabled').get('href')
+    except Exception:
+        pageNextBtn = ''
     # print(allJobs)
-    allJobsDf = pd.DataFrame(allJobs)
-    print(allJobsDf)
-    
-    allJobsDf.to_csv('jobs.csv',encoding='utf-8-sig')
-    
 
+    # Export to CSV later DB
+    # allJobsDf = pd.DataFrame(allJobs)
+    # allJobsDf.to_csv('jobs.csv',encoding='utf-8-sig')
+    print('Ending page')
 ######
 
 
@@ -47,7 +77,7 @@ def scrapePage(catLink):
 url = 'https://elbitsystemscareer.com/'
 categoryClass = "item-practice"
 
-# 
+#
 source = requests.get(
     url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}).text
 
@@ -55,20 +85,21 @@ soup = BeautifulSoup(source, 'html.parser')
 
 categories = soup.find_all('div', class_=categoryClass)
 
-print(categories)
-
+# print(categories)
 
 
 # ===============================TESTING AREA ===============================
 # Testing all categories
-# 
+#
 # for caterogy in categories:
 #     scrapeCategory(caterogy)
 #     print('----------')
 
 # Testing one category
-# 
-scrapeCategory(categories[0])
+#
+for i in range(3):
+    print(i)
+    scrapeCategory(categories[i])
 
 # print(df)
 
